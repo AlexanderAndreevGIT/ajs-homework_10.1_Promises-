8d8052f1fb1b74465c2b8df9bb9acfdad43cc88c
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2021-11-25 20:14:29 +0300

    initial commit

---
+++ src/js/gamesavingloader.js
@@ -0,0 +1,11 @@
+import read from './reader';
+import json from './parser';
+
+export default class GameSavingLoader {
+  static load() {
+    return new Promise((resolve) => {
+      resolve(read());
+    })
+      .then((result) => json(result));
+  }
+}
